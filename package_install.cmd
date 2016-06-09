1400e9a6ad68a76271899cdc4dae8d215358aad0
egor ol't <olt.egor@gmail.com>
2016-06-09 21:04:18 +0300

    bower fix

--- package_install.cmd
+++
@@ -1,4 +0,0 @@
-SET dir=bower_components
-powershell.exe -f abProject.ps1 -dir %dir% -packageName connectionStatus
-powershell.exe -f abProject.ps1 -dir %dir% -packageName localStorageCache
-powershell.exe -f abProject.ps1 -dir %dir% -packageName indexedDbCache
